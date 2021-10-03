5bdc472940023ac5ca44b81919b5ebddb75f637e
Gabriel Brum <biel.brumluz@gmail.com>
2021-10-03 14:39:30 -0300

    version 1.0.1

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div class="container">
       <header>
-        <img src="" alt="Imperador Palpatine">
+        <img src="assets/imperador.png" alt="imperador palpatine">
         <h2>Precisamos derrotar o imperador palpatine!</h2>
         <h3>Cadastre-se para ser um aliado</h3>
       </header>
@@ -35,8 +35,8 @@
             <td>{{ candidato.birthDate }}</td>
             <td>{{ candidato.description }}</td>
             <td>
-              <button class="waves-effect btn-small blue darken1"><i class="material-icons">create</i></button>
-              <button class="waves-effect btn-small red darken1"><i class="material-icons">delete_sweep</i></button>
+              <button @click="editar(candidato)" class="waves-effect btn-small blue darken1"><i class="material-icons">create</i></button>
+              <button @click="remover(candidato)" class="waves-effect btn-small red darken1"><i class="material-icons">delete_sweep</i></button>
             </td>
           </tr>
         </tbody>
@@ -53,33 +53,68 @@
     data(){
       return {
         candidato:{
+          id: '',
           name: '',
           planet: '',
           birthDate: '',
           description: ''
         },
 
-        candidatos: [
-
-        ]
+        candidatos: [],
+        errors: []
       }
     },
 
     mounted(){
-      Candidatos.listar().then(resposta => {
-        console.log(resposta.data)
-        this.candidatos = resposta.data
-      })
+      this.listar()
     },
 
     methods:{
 
+      listar(){
+        Candidatos.listar().then(resposta => {
+          this.candidatos = resposta.data
+        })
+      },
+
       salvar(){
 
-        // Candidatos.salvar(this.candidato).then(resposta => {
-        //   alert("Salvo com sucesso!")
-        // })
+        if(!this.candidato.id){
+          Candidatos.salvar(this.candidato).then(resposta => {
+          this.candidato = {}
+          resposta.alert("Salvo com sucesso!")
+          this.listar()
+          this.errors = []
+          }).catch(e =>{
+            this.errors = e.response.data.errors
+          })
 
+        }else{
+          Candidatos.atualizar(this.candidato).then(resposta => {
+          this.candidato = {}
+          resposta.alert("Atualizado com sucesso!")
+          this.listar()
+          this.errors = []
+          }).catch(e =>{
+          this.errors = e.response.data.errors
+          })
+        }
+      },
+
+      editar(candidato){
+        this.candidato = candidato 
+      },
+
+      remover(candidato){
+
+        if(confirm("Realmente deseja excluir o candidato?")){
+          Candidatos.apagar(candidato).then(resposta =>{
+          resposta.this.listar()
+          this.errors = []
+          }).catch(e =>{
+            this.errors = e.response.data.errors
+          })
+        }
       }
 
     }
@@ -88,5 +123,130 @@
 </script>
 
 <style>
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body{
+    background-image: url('assets/fundo_galaxia.png');
+    background-size: cover;
+}
+
+.container{
+    width: 60%;
+    margin: auto;
+}
+
+@font-face {
+    font-family: Star Jedi;
+    src: url(fonts/starjedi/Starjedi.ttf);
+}
+
+header{
+    text-align: center;
+    font-family: Star Jedi;
+    color: white;
+    letter-spacing: 2px;
+}
+
+header img{
+    width: 60%;
+}
+
+header h2{
+    margin-top: 20px;
+    font-size: 24px;
+}
+
+header h3{
+    font-size: 18px;
+}
+
+form{
+    margin: 50px 0;
+    text-align: center
+}
+
+form input::placeholder{
+    color: white;
+}
+
+form textarea::placeholder{
+    color: white;
+}
+
+form input{
+    width: 100%;
+    height: 45px;
+    border-radius: 5px;
+    color: white;
+    padding-left: 15px !important;
+    border: 2px solid white;
+    outline: none;
+    letter-spacing: 2px;
+    font-family: Star Jedi;
+    margin-bottom: 10px;
+}
+
+form textarea{
+    width: 102%;
+    height: 150px;
+    border-radius: 5px;
+    color: white;
+    padding: 10px 0px 0px 15px;
+    border-bottom: 2px solid white;
+    outline: none;
+    letter-spacing: 2px;
+    font-family: Star Jedi;
+    resize: none;
+    margin-bottom: 30px;
+}
+
+form .btn{
+    width: 30%;
+    height: 45px;
+    border-radius: 5px;
+    outline: none;
+    background-color: white !important;
+    color: black;
+    line-height: 45px;
+    text-align: center;
+    font-family: Star Jedi;
+    letter-spacing: 2px;
+}
+
+.btn i{
+    color: black;
+}
+
+table{
+    color: white;
+    text-align: center;
+}
+
+table thead{
+    font-size: 14px;
+    font-family: Star Jedi;
+    letter-spacing: 2px;
+    padding: 0 10px;
+}
+
+@media screen and (max-width: 768px){
+    .container{
+        width: 90%;
+    }
+
+    table thead{
+        font-size: 12px;
+    }
+}
+
+@media screen and (max-width: 568px){
+    .container{
+        width: 95%;
+    }
+}
 
 </style>
--- src/service/candidatos.js
+++ src/service/candidatos.js
@@ -8,5 +8,13 @@ export default {
 
     salvar:(candidato) =>{
         return http.post('candidato', candidato)
+    },
+
+    atualizar:(candidato) =>{
+        return http.put('candidato', candidato)
+    },
+
+    apagar: (candidato) =>{
+        return http.delete('candidato', { data: candidato }) 
     }
 }
